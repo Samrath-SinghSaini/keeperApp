ebd615ebfa1e1206d875fd5890274be6f561445f
Samrath Singh Saini <62909539+SamrathSinghSaini@users.noreply.github.com>
2023-10-28 22:21:03 -0700

    vercel settings and ran build command

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 let BASEURL = 'https://keeper-app-six-sigma.vercel.app/'
-import s from './src/public'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build:{
