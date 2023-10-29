e3cb4fe935b8a16dd67ae3af178c2341d081f96a
Samrath Singh Saini <62909539+SamrathSinghSaini@users.noreply.github.com>
2023-10-29 15:50:44 -0700

    changing backend from vercel to render

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-let BASEURL = 'https://keeper-app-six-sigma.vercel.app/'
+let BASEURL = 'https://keeperappbackend-ijry.onrender.com//'
 
 // https://vitejs.dev/config/
 export default defineConfig({
