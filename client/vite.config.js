92bebb50ca748affadacdfb80a3acdb47f44cfcf
Samrath Singh Saini <62909539+SamrathSinghSaini@users.noreply.github.com>
2023-10-28 22:19:52 -0700

    updated project config files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-let BASEURL = 'http://localhost:3000'
+let BASEURL = 'https://keeper-app-six-sigma.vercel.app/'
+import s from './src/public'
 // https://vitejs.dev/config/
 export default defineConfig({
   build:{
     manifest:true, 
     rollupOptions:{
       input:'./src/App.jsx'
-    }
+    },
+    outDir:"./src/public"
   },
   server: {
     proxy: {
@@ -28,35 +30,8 @@ export default defineConfig({
         changeOrigin: true,
         secure: false, 
       }
-      // },
-      // '/post': {
-      //   target: 'http://localhost:3000/',
-      //   changeOrigin: true,
-      //   secure: false, 
-      //   rewrite: (path) => path.replace(/^\/post/, '/post'),
-      // },
-      // '/delete':{
-      //   target:'http://localhost:3000/', 
-      //   changeOrigin: true,
-      //   secure: false, 
-      //   rewrite: (path) => path.replace(/^\/delete/, '/delete'),
-      // },
-      // Using the proxy instance
-      // '/app': {
-      //   target: 'http://localhost:3000/',
-      //   changeOrigin: true,
-      //   configure: (proxy, options) => {
-      //     // proxy will be an instance of 'http-proxy'
-      //   }}
+      
     },
-  plugins: [react()],
+  plugins: [react()]
 }})
 
-// ,
-//         '/post': {
-//           target: 'http://localhost:3000/',
-//           changeOrigin: true,
-//           configure: (proxy, options) => {
-//             // proxy will be an instance of 'http-proxy'
-//           },
-//       }
