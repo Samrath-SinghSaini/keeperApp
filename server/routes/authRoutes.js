a01002023836960ea55ec8691e822564e5bc7944
Samrath Singh Saini <62909539+SamrathSinghSaini@users.noreply.github.com>
2023-10-29 22:23:03 -0700

    changed register routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -24,7 +24,7 @@ router.get("/notes", async (req, res) => {
     });
 });
 
-router.post("/", async (req, res) => {
+router.post("/register", async (req, res) => {
   let newUserObj = req.body;
   //searching db for username
   let findArray = await userModel.find({ userName: newUserObj.userName });
