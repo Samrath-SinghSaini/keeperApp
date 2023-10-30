04c883fb64cd3191c27d82323f9edcb9e65f0f16
Samrath Singh Saini <62909539+SamrathSinghSaini@users.noreply.github.com>
2023-10-29 17:10:57 -0700

    proxy settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-let BASEURL = 'https://keeperappbackend-ijry.onrender.com//'
+let BASEURL = 'https://keeperappbackend-ijry.onrender.com/'
 
 // https://vitejs.dev/config/
 export default defineConfig({
